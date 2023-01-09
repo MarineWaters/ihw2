922a0d69cabab35e65c1fb48e1f844bab5514443
Juliana Rodionova <90824067+MarineWaters@users.noreply.github.com>
2023-01-09 23:23:44 +0300

    Update Main.java
    Updated Javadoc and changed some code. Now both cycled and normal files will be shown.

--- ihw2/src/com/hw/filedependency/Main.java
+++ ihw2/src/com/hw/filedependency/Main.java
@@ -3,10 +3,14 @@ package com.hw.filedependency;
 import java.io.IOException;
 
 /**
- * Основной класс с единственным методом. Проверяется зацикленность, если её нет, выводится текст и список.
- * При наличии IOException выводится отдельное сообщение, так как здесь это наиболее вероятное исключение.
+ * Основной класс с единственным методом.
  */
 public class Main {
+    /**
+     * Проверяется зацикленность, если её нет, сразу выводятся список и текст.
+     * Если есть зацикленность, сначала выводятся файлы с ней. Список и текст не учитывают такие файлы.
+     * При наличии IOException выводится отдельное сообщение, так как здесь это наиболее вероятное исключение.
+     */
     public static void main(String[] args) {
         System.out.println("Checking Catalogue for files with dependencies...");
         try {
@@ -14,10 +18,9 @@ public class Main {
             FileContent content = new FileContent();
             if (content.filesAreLooped(check.files)) {
                 check.loopedCheck(content.looped);
-            } else {
-                content.concatenation();
-                check.normalCheck(content.allFiles, String.valueOf(content.text));
             }
+            content.concatenation();
+            check.normalCheck(content.allFiles, String.valueOf(content.text));
         } catch (IOException e) {
             System.out.println("It seems there are file access problems.");
         } catch (Exception e) {
